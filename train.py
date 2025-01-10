1d31ccd7e669517ad3b36a45809681b7261a68d8
kavorite <kavorite@gmail.com>
2025-01-09 23:56:12 -0500

    various
    - add main function
    - fix shaping bugs

--- train.py
+++ train.py
@@ -26,7 +26,12 @@ def pad_to_multiple_of(a, divisor, axis=-1):
 
 
 def batches():
-    df = pl.scan_parquet("gtzan.parquet").select(
+    if not os.path.exists("gtzan"):
+        import subprocess as sp
+
+        sp.run("fetch.sh")
+
+    df = pl.scan_parquet("gtzan/*.parquet").select(
         decode_wav(pl.col("audio").struct.field("bytes")).alias("audio"), "genre"
     )
     loaded = ak.from_arrow(df.collect().to_arrow())
@@ -51,39 +56,42 @@ def batches():
         yield chunk
 
 
-max_lr = 1e-3
-scheduler = torch.optim.lr_scheduler.OneCycleLR(
-    optimizer,
-    max_lr=max_lr,
-    total_steps=total_steps,
-    final_div_factor=1e4,
-    anneal_strategy="cos",
-)
+def main():
+    max_lr = 1e-3
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=max_lr,
+        total_steps=total_steps,
+        final_div_factor=1e4,
+        anneal_strategy="cos",
+    )
+
+    running_loss = 0.0
+    loss_beta = 1 - 1 / 128
+
+    for step, batch in islice(enumerate(batches()), total_steps):
+        optimizer.zero_grad()
+        logits = model(batch["audio"].to(torch.bfloat16).to("cuda"), batch["tk_id"])
+        loss = F.cross_entropy(
+            logits, F.one_hot(batch["genre"].to(device="cuda", dtype=torch.bfloat16))
+        ).mean()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        with torch.no_grad():
+            display_loss = loss.item()
+            if step == 0:
+                running_loss = display_loss
+            else:
+                attenuation = 1 - (1 - loss_beta) ** (step + 1)
+                running_loss = (
+                    loss_beta * running_loss + (1 - loss_beta) * display_loss
+                ) / attenuation
+        print(f"Step {step}: {running_loss:.3g}" + 32 * " ", end="\r")
+
+    save_model(model, "model.safetensors")
+    print("Saved to model.safetensors")
 
 
-running_loss = 0.0
-loss_beta = 1 - 1 / 128
-
-
-for step, batch in islice(enumerate(batches()), total_steps):
-    optimizer.zero_grad()
-    logits = model(batch["audio"].to(torch.bfloat16).to("cuda"), batch["tk_id"])
-    loss = F.cross_entropy(
-        logits, F.one_hot(batch["genre"].to(device="cuda", dtype=torch.bfloat16))
-    ).mean()
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-    with torch.no_grad():
-        display_loss = loss.item()
-        if step == 0:
-            running_loss = display_loss
-        else:
-            attenuation = 1 - (1 - loss_beta) ** (step + 1)
-            running_loss = (
-                loss_beta * running_loss + (1 - loss_beta) * display_loss
-            ) / attenuation
-    print(f"Step {step}: {running_loss:.3g}" + 32 * " ", end="\r")
-
-save_model(model, "model.safetensors")
-print("Saved to model.safetensors")
+if __name__ == "__main__":
+    main()
